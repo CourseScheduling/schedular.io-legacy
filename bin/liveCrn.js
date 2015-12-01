eaf7e26d91f07d3ff3aaa45857440651ad0ebc5a
Joseph Thomas <joey6978@gmail.com>
2015-12-01 00:05:17 -0800

    Added fillData protocol to the liveCRN module

--- bin/liveCrn.js
+++ bin/liveCrn.js
@@ -1,7 +1,9 @@
 var Mem = require('memcached');
 //var mem =   new memcached('schedular.luawsd.cfg.usw2.cache.amazonaws.com:11211');
 var memServer=new Mem('127.0.0.1:11211');
+//Variables for CRN and memcached
 var memCRNUpdateKey =   "CRN_UPDATE_KEY";
+var CRNPrefix   =   "CRN-";
 
 var app =   {
     timeOut:{},
@@ -19,13 +21,23 @@ app.init   =   function(){
     
     // Grab the last update time.
     memServer.get(memCRNUpdateKey,function(e,data){
+        //Set the lastUpdate variable
         global.lastUpdateCRN    =   data;
+        //Set the timeout function to go off after 10min since last update on memcached side.
+        setTimeout(app.timeOut,(60000+Date.now())-data);
     });
 };
 
 app.timeOut =   function(){
     memServer.get(memCRNUpdateKey,function(e,data){
-        if(data
+        //GRAB ALL CRNS and store in fillData
+        
+        memcached.getMulti(CRNarray, function (err, data) {
+            //Populate the global fillData object with crns and their corresponding data.
+            for(var crn in data){
+                global.fillData[crn.substr(CRNPrefix.length)]   =   data[crn];
+            }
+        });
     });
 }
 
