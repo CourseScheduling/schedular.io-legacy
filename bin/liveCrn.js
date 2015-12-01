6f88573eb0208b14c986491703d6193d825a6cce
Joseph Thomas <joey6978@gmail.com>
2015-11-30 23:47:12 -0800

    Created init and timeout functions for liveCRN module

--- bin/liveCrn.js
+++ bin/liveCrn.js
@@ -1,6 +1,33 @@
-var mem = require('memcached');
-var app =   {};
+var Mem = require('memcached');
+//var mem =   new memcached('schedular.luawsd.cfg.usw2.cache.amazonaws.com:11211');
+var memServer=new Mem('127.0.0.1:11211');
+var memCRNUpdateKey =   "CRN_UPDATE_KEY";
 
+var app =   {
+    timeOut:{},
+    start:{}
+};
+
+app.init   =   function(){
+    
+    // Create a global socket object.
+    global.sockets          =   {};
+    global.fillData         =   {};
+    
+    global.lastUpdateCRN    =   Date.now();
+    var timeOutTime =   {}
+    
+    // Grab the last update time.
+    memServer.get(memCRNUpdateKey,function(e,data){
+        global.lastUpdateCRN    =   data;
+    });
+};
+
+app.timeOut =   function(){
+    memServer.get(memCRNUpdateKey,function(e,data){
+        if(data
+    });
+}
 
 
 
