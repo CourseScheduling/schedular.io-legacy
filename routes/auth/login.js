a41b0cc3abb67a9046925be6c86efc91c0447d90
Joseph Thomas <joey6978@gmail.com>
2015-12-10 11:29:01 -0800

    Re-writing/modularizing all server side stuff

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -9,7 +9,9 @@ var DB      =   require('../../bin/db.js');
 
 var CORE    =   {
     validate:{},
-    user:{},
+    user:{
+        getUser:{}
+    },
     session:{}
 }
 
@@ -17,52 +19,96 @@ var CORE    =   {
     Validate, anything that involves validating inputs    
 */
 
-CORE.validate   =   function(username,password,cb){
-    var searchSQL   =   'SELECT userId,password FROM userlogin WHERE(username=?)';   
-    DB.query(searchSQL,[username],function(err,result){
-        //return if user does not exist 
-        if(result.length==0)
-            return cb&&cb(false,'BAD_USERNAME');
-        //get the hash of the inputted password
-        var hash    =   crypto.createHash('sha512').update(result[0].password.slice(0,10)+p).digest('base64');
-        //compare the newhash to the dbhash
-        if(hash===result[0].password.substr(10)){
-            result[0].username = username;
-            cb&&cb(true,result[0]);
-        }else{
-            cb&&cb(false,'WRONG_PASSWORD');
-        }
+CORE.validate   =   (function(CORE){
+    
+    return function(username,password,cb){
+        var searchSQL   =   'SELECT userId,active,password FROM userlogin WHERE(username=?)';   
+        DB.query(searchSQL,[username],function(err,result){
+            //return if user does not exist 
+            if(result.length==0)
+                return cb&&cb(false,'BAD_USERNAME');
+            //get the hash of the inputted password
+            var hash    =   crypto.createHash('sha512').update(result[0].password.slice(0,10)+password).digest('base64');
+            //compare the newhash to the dbhash
+            if(hash===result[0].password.substr(10)){
 
-    });
-}
+                //See how active the account is.             
+                switch(result[0].active){
+                    case -1:
+                    case 0:
+                        cb&&cb(false,'ACTIVATION_NEEDED |'+username);
+                    break;
+                    case 1:
+                        result[0].username = username;
+                        cb&&cb(true,result[0]);
+                    break;
+                }
+
+            }else{
+                cb&&cb(false,'WRONG_PASSWORD');
+            }
+
+        });
+    }
+})(CORE);
+
+/*
+
+    User, anything with retrieving user info
+    
+*/
+
+CORE.user.getUser   =   (function(CORE){
+    return function(userId,cb){
+        var searchSQL   =   'SELECT id,studentNumber,firstname,lastname,title,accountType FROM user WHERE id=?'
+        DB.query(searchSQL,userId,function(err,results){
+            cb&&cb(results);
+        });
+    }
+})(CORE);
+
 
 /*
     Session, anything involving adding stuff and taking stuff away from sessions
 */
 
-CORE.session = {
-    addData:function(req,data,cb){
-        req.session.userData    =   data;
-        req.session.loggedIn    =   true;
-        req.session.save(function(e){
-            cb&&cb();
-        });
+
+
+CORE.session = (function(CORE){
+    return {
+        set:function(req,data,cb){
+            req.session.userData    =   data;
+            req.session.loggedIn    =   true;
+            req.session.save(function(e){
+                cb&&cb();
+            });
+        }
     }
-}
+})(CORE);
+
+/*
+    Routes, now here comes the main part of our program
+*/
+
 
 router.post('/',function(req,res,next){
+    //convert username to lowercase
     req.body.u  =   req.body.u.toLowerCase();
-    CORE.validate(req.body.u,req.body.p,function(good,reason){
+    CORE.validate(req.body.u,req.body.p,function(good,data){
+        //If the function called the callback with a false first parameter tell the user the reason.
         if(!good){
-            res.send([reason]);
-            return;
+            res.send([data]);
+            return;   
         }
         
-        UserMod.grabUser(reason,function(data){
-            SessionMod.addData(req,data,function(){
-                res.send(['SUCCESS']);
+        //Get the userdata
+        CORE.user.getUser(data.userId,function(data){
+            //add username to the data
+            data.username   = req.body.u;
+            CORE.session.set(req,data,function(){
+                res.send('["SUCCESS"]');
             });
-        });
+        }); 
     });
 });
 
