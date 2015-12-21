be6b992b24814ebbf128deec9994151f31d82e45
Joseph Thomas <joey6978@gmail.com>
2015-12-20 21:06:59 -0800

    ugh, design for review making me mad

--- routes/review/course.js
+++ routes/review/course.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 
+function jadeData(req){
+    return {
+            firstname:req.session.userData.firstname,
+            lastname:req.session.userData.lastname,
+            student:req.session.userData.studentNumber,
+            username:req.session.userData.username
+           }
+}
+
 router.get('/', function(req, res, next) {
-    res.render('review/course');
+    res.render('review/course',jadeData(req));
 });
 
 
