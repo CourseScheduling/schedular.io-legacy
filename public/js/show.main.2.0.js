105700c391b1dc5a79f3d5b29522a888186b80ee
Joseph Thomas <joey6978@gmail.com>
2016-01-02 18:08:25 -0800

    Finished scheduling part of client i.e. show.main

--- public/js/show.main.2.0.js
+++ public/js/show.main.2.0.js
@@ -1,10 +1,32 @@
 CORE	=	{
 	raw	:	RAW_COURSE_DATA,
+	courses	:	[],
 	helper	:	{},
 	main	:	{
 		search	:	{}
 	}
 }
+CORE.helper.time	=	(function(CORE){
+	return {
+		inTime:function(start,end,start2,end2){
+				if(start==0||start2==0)
+						return false;
+				if((((start2>=start)&&(start2<end))||((end2>start)&&(end2<end)))||(((start>=start2)&&(start<end2))||((end>start2)&&(end<end2))))
+						return true;
+				return false;
+		},
+		sameArr:function(day1,day2){
+				if(day1[0]==-1||day2[0]==-1)
+								return false;
+				for (var i = 0; i < day1.length; i++) {
+						if (day2.indexOf(day1[i]) > -1) {
+								return true;
+						}
+				}
+				return false;
+		}
+	};
+})(CORE.helper);
 
 CORE.helper.array	=(function(HELPER){
 	return {
@@ -23,32 +45,85 @@ CORE.helper.array	=(function(HELPER){
 CORE.main.search	=	(function(CORE){
 	return {
 		prep:function(){
+			var mangledArray	=	[];
 			CORE.raw.forEach(function(course){
 				var typeArray	=	[];
 				for(var type in course.sections){
 					typeArray.push(course.sections[type]);
 				}
 				typeArray.sort(function(a,b){return b.length-a.length;});
-				
+
+				var mangla	=	[];
 				course.mangled.forEach(function(v,i,a){
-					a[i]	=	v.map(function(uniq){
+					var mJ	=	{times:[]};
+					v.forEach(function(uniq,i){
 						var i = 0;
 						var found = false;
 						while(!found&&i<3){
 							found	=	CORE.helper.array.returnByField(typeArray[i++],'uniq',uniq);
 						}
-						return found;
+						mJ[i]	=found;
+						mJ.times	=	mJ.times.concat(found.times);
+						mJ.length = i;
 					});
-					
+					mangla.push(mJ);
 				});
+				mangledArray.push(mangla);
 			});
+			mangledArray.sort(function(a,b){return b.length-a.length;});
+			return mangledArray;
 		},
-		check:function(mangle1,mangle2){
+		start:function(){
+			CORE.courses[CORE.courses.length-1].forEach(function(section,index){
+				console.log(section);
+				depth([section],CORE.courses.length-2);
+			});
+				return possible;
+		},
 			
-		},
-		mangle:function(sectionArray){
+		check:function(mangle1,mangle2){
+				
 		}
 	};
 })(CORE);
 
 
+			var possible	=	[];
+
+			function depth(pos,index){
+				if(index<0)
+					return possible.push(pos);
+				var course = CORE.courses[index];
+				var layerI	=	CORE.courses[index].length;
+				up:
+				while(layerI--){
+					while(posI--){
+						var timeP	=	pos[posI].times.length;
+						while(timeP--){
+							var timeL	=	course[layerI].times.length;
+							while(timeL--){
+									
+								var tP	=	pos[posI].times[timeP];
+								var tL	=	course[layerI].times[timeL];
+								if(CORE.helper.time.inTime(tP.start,tP.end,tL.start,tL.end)&&CORE.helper.time.sameArr(tP.days,tL.days)){
+									continue up;
+								}
+								
+							}
+						}
+						
+					}
+					if(index==0)
+						possible.push(pos.concat(course[layerI]));
+					else
+						depth(pos.concat(course[layerI]),index-1);
+				}
+			}
+var d = new Date();
+CORE.courses	=	CORE.main.search.prep();
+console.log('Prep took: '+((new Date())-d)+'ms');
+
+
+var d = new Date();
+	CORE.main.search.start();
+console.log('Scheduling took: '+((new Date())-d)+'ms');
